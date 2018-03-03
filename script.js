7528e7f661b77bcbedaa0cbd328b7f30e8f6ee42
Kevin Litchfield <kevinlitchfield@users.noreply.github.com>
2018-03-03 15:34:07 -0500

    Improvements

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function renderIndex() {
   Object.keys(localStorage).forEach(function (id) {
     thread = JSON.parse(localStorage.getItem(id))
     if (thread !== null) {
-      rendered = postTemplate({title: thread.title, author: thread.by, time: thread.time, id: thread.id, commentsCount: thread.descendants});
+      rendered = postTemplate({title: thread.title, author: thread.by, time: timeAgo(thread.time), id: thread.id, commentsCount: thread.descendants});
       contentDiv.insertAdjacentHTML('beforeend', rendered);
     }
   })
@@ -44,7 +44,7 @@ function renderThread(threadId) {
 function renderKids(item) {
   var parentDiv = document.getElementById('item-' + item.id + '-kids');
   item.kids.forEach(function(kid) {
-    rendered = commentTemplate({title: kid.title, commentHtml: kid.text, author: kid.by, time: kid.time, id: kid.id});
+    rendered = commentTemplate({title: kid.title, commentHtml: kid.text, author: kid.by, time: timeAgo(kid.time), id: kid.id});
     parentDiv.insertAdjacentHTML('beforeend', rendered);
     if (kid.kids) {
       renderKids(kid)
@@ -66,13 +66,35 @@ function renderArticle(threadId) {
   clearContentDiv()
   var contentDiv = document.getElementById('content');
   thread = JSON.parse(localStorage.getItem(threadId))
-  rendered = articleTemplate({title: thread.title, articleHtml: thread.summary});
+  rendered = articleTemplate({title: thread.title, articleHtml: thread.summary, id: thread.id});
   contentDiv.insertAdjacentHTML('beforeend', rendered);
 }
 
 
 // convert timestamp to hn style "x unit ago"
-function timeAgo(timestamp) {}
+function timeAgo(timestamp) {
+  console.log({
+    now: new Date().getTime(),
+    ts: timestamp
+  })
+  var seconds = Math.floor((new Date().getTime() - timestamp * 1000) / 1000)
+  var minutes = Math.floor(seconds / 60)
+  var hours = Math.floor(minutes / 60)
+  var days = Math.floor(hours / 24)
+  var years = Math.floor(days / 365)
+
+  if (years) {
+    return years + " years ago"
+  } else if (days) {
+    return days + " days ago"
+  } else if (hours) {
+    return hours + " hours ago"
+  } else if (minutes) {
+    return minutes + " minutes ago"
+  } else if (seconds) {
+    return seconds + " seconds ago"
+  }
+}
 
 
 
